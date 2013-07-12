3319a79fdfd1b20f14f281c329ed09322fd5fb18
Hunter Francis <francis1122@gmail.com>
2013-07-11 19:36:14 -0700

    Initial Commit

---
+++ FingerJoust/AppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.h
+//  FingerJoust
+//
+//  Created by Hunter Francis on 7/11/13.
+//  Copyright Hunter Francis 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+// Added only for iOS 6 support
+@interface MyNavigationController : UINavigationController <CCDirectorDelegate>
+@end
+
+@interface AppController : NSObject <UIApplicationDelegate>
+{
+	UIWindow *window_;
+	MyNavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) MyNavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ FingerJoust/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  FingerJoust
+//
+//  Created by Hunter Francis on 7/11/13.
+//  Copyright Hunter Francis 2013. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
